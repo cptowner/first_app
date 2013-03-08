339a516044d837de913324c21230cdb0171c8dc8
cptowner <cptowner@yahoo.com>
2013-03-08 06:33:55 -0600

    Add remainder of first_app folders.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f5ab96143bb5c06d1d744e658a25d96a7a2e891a24aa6c170ca71dab5c7189d128a3f5e597eefc0d5892bf3f90a1d2fc582fe1a3cc5b36ab417b63507dc5ea6b'
